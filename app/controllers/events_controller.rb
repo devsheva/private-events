7708125fe5ffa24ef0acf752085d732038568055
Mateo Sheshi <sheshi.mateo01@gmail.com>
2022-09-15 15:07:09 +0200

    feat: Events and Users

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,27 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  def event_params
+    params.require(:event).permit(:date, :location)
+  end
+end
---
+++ db/migrate/20220915121955_add_fk_events.rb
@@ -0,0 +1,5 @@
+class AddFkEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key "events", "users", column: "creator_id"
+  end
+end
